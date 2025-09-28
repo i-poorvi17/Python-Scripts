7d2519849660886b737e9051572e8d23b7ff6f2e
Poorvi Agrawal <poorvi.pics17@gmail.com>
2025-09-28 20:31:13 +0530

    Create translate.py

---
+++ translate.py
@@ -0,0 +1,7 @@
+translate={}
+while True:
+    print("enter the words in the form 'namaste':'greetings\n")
+    x=input("enter the hindi word\t")
+    y=input("enter the translation in english\t")
+    translate.update({x:y})
+    print(translate)
