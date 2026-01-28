846398178eb0a4fe26c0648f2b003c17c8e8ecf8
Poorvi Agrawal <poorvi.pics17@gmail.com>
2026-01-28 20:18:48 +0530

    Create kbc.py

---
+++ kbc.py
@@ -0,0 +1,7 @@
+#Ques={"complete this idiom: ghar ka bhedi _______ dhaye":"lanka"}
+print('\n')
+x=input(" complete the phrase : ghar ka beedi _____ dhaye \n 1.lanka \t 2.dhaka \n 3. patna \t 4. hathras \n  ")
+if (x=="1"):
+    print("correct answer , you win ₹10,000")
+else:
+    print("wrong answer you lose")
