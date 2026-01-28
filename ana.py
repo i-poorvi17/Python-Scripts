a9155ed2a84458a781475df5c2b748e5cb8ad011
Poorvi Agrawal <poorvi.pics17@gmail.com>
2026-01-28 20:17:53 +0530

    Create ana.py

---
+++ ana.py
@@ -0,0 +1,8 @@
+a=input('enter the number')
+b=input('enter the number')
+if (sorted(a)==sorted(b)):
+    print("anagram!!!!")
+else:
+    print("not anagram")
+
+    print(f'hey my name is {a}and i m from {b}')
